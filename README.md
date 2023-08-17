d896292c060e70d5fb9f646414a75dd92f3922ef
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2023-08-17 15:31:25 +0600

    Update

---
+++ README.md
@@ -0,0 +1,24 @@
+# View MD
+
+**View MD** is a simple tool to view markdown files in windows.
+
+## Table of Contents
+
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [How to use](#how-to-use)
+* [Features](#features)
+* [Status](#status)
+* [Sources](#sources)
+* [Contact](#contact)
+* [License](#license)
+* [Screenshots](#screenshots)
+* [Credits](#credits)
+* [Inspiration](#inspiration)
+* [Author](#author)
+* [Contributing](#contributing)
+
+## General info
+
+**View MD** is a simple tool to view markdown files in windows.
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
-from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QFileDialog
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 import mistune
 
 
-def out(markdown):
+def convert_to_html(markdown):
     return mistune.html(markdown)
 
 
@@ -12,7 +12,7 @@ class HTMLViewer(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("HTML Viewer")
+        self.setWindowTitle("View MD - Markdown Viewer")
         self.setGeometry(100, 100, 800, 600)
 
         self.central_widget = QWidget()
@@ -23,14 +23,23 @@ class HTMLViewer(QMainWindow):
         self.web_view = QWebEngineView()
         layout.addWidget(self.web_view)
 
-        # Load and display HTML content
-        self.md = "# List\n" \
-                  "* Item 1\n" \
-                  "* Item 2"
-        self.output = out(self.md)
-        self.load_html_content(self.output)
+        self.open_button = QPushButton("Open .md File")
+        layout.addWidget(self.open_button)
 
-    def load_html_content(self, html_content):
+        self.open_button.clicked.connect(self.open_md_file)
+
+    def open_md_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open .md File", "", "Markdown Files (*.md)", options=options)
+
+        if file_name:
+            with open(file_name, "r", encoding="utf-8") as f:
+                md_content = f.read()
+                self.display_html(md_content)
+
+    def display_html(self, markdown):
+        html_content = convert_to_html(markdown)
         self.web_view.setHtml(html_content)
 
 
@@ -38,4 +47,4 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     viewer = HTMLViewer()
     viewer.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
