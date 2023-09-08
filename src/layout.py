19af301ecb09ddbc0e801f52879ab4b1ce9838e5
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2023-09-08 23:38:32 +0600

    Update

--- src/layout.py
+++ src/layout.py
@@ -15,5 +15,74 @@
 """
 
 
-def layout(filename, content):
-	return content
+def layout(file_name, content):
+	template = '''<style>
+	body {
+		display: grid;
+	}
+
+	main {
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Noto Sans", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+		background-color: white;
+		margin: 20px;
+		border: 1px solid #D0D7DE;
+		border-radius: 10px;
+		/*box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);*/
+	}
+
+	main .title {
+		border-bottom: 1px solid #D0D7DE;
+		padding: 10px 30px;
+	}
+
+	main .title * {
+		margin-right: 5px;
+	}
+
+	main .content {
+		padding: 0 30px 10px 30px;
+	}
+
+	main > .content h1, main > .content h2, main > .content h3, main > .content h4, main > .content h5, main > .content h6 {
+		border-bottom: 1px solid #D0D7DE;
+		padding-bottom: 10px;
+		margin-top: 30px;
+	}
+
+	main > .content a:link, main > .content a:visited {
+		color: #218BFF;
+		text-decoration: none;
+	}
+
+	main > .content a:hover {
+		text-decoration: underline;
+	}
+
+	main > .content pre {
+		background-color: #F6F8FA;
+		padding: 20px;
+		border-radius: 5px;
+	}
+
+	@media only-screen and (min-width: 768px) {
+		aside {
+			display: block;
+		}
+	}
+</style>
+
+<main>
+	<div class="title">
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-markdown" viewBox="0 0 16 16">
+			<path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"/>
+			<path fill-rule="evenodd" d="M9.146 8.146a.5.5 0 0 1 .708 0L11.5 9.793l1.646-1.647a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 0-.708z"/>
+			<path fill-rule="evenodd" d="M11.5 5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 1 .5-.5z"/>
+			<path d="M3.56 11V7.01h.056l1.428 3.239h.774l1.42-3.24h.056V11h1.073V5.001h-1.2l-1.71 3.894h-.039l-1.71-3.894H2.5V11h1.06z"/>
+		</svg>
+		<span><strong>''' + file_name + ''''</strong></span>
+	</div>
+	<div class="content">
+		''' + content + '''
+	</div>
+</main>'''
+	return template
--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@
 """
 
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QFileDialog
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QFileDialog, QStyle
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 import mistune
 import layout
@@ -25,15 +25,11 @@ def convert_to_html(markdown):
 	return mistune.html(markdown)
 
 
-def sidebar():
-	return ''
-
-
-class HTMLViewer(QMainWindow):
+class ViewMD(QMainWindow):
 	def __init__(self):
 		super().__init__()
 
-		self.setWindowTitle("View MD - Markdown Viewer")
+		self.setWindowTitle('View MD - Markdown Viewer')
 		self.setGeometry(300, 100, 800, 600)
 
 		self.central_widget = QWidget()
@@ -44,13 +40,90 @@ class HTMLViewer(QMainWindow):
 		self.web_view = QWebEngineView()
 		layout.addWidget(self.web_view)
 
-		self.open_button = QPushButton("Open .md File")
+		self.open_button = QPushButton('Open .md File')
+		self.open_button.setStyleSheet(
+			'QPushButton { font-size: 14px; padding: 10px; }'
+		)
 		layout.addWidget(self.open_button)
 
-		self.open_button.clicked.connect(self.open_md_file)
+		self.open_button.clicked.connect(self.open_file)
 
-	def open_md_file(self):
-		file_name, _ = QFileDialog.getOpenFileName(self, "Open .md File", "", "Markdown Files (*.md)")
+		# Menu bar
+		menu_bar = self.menuBar()
+
+		# Menu items
+		file_menu = menu_bar.addMenu('File')
+		help_menu = menu_bar.addMenu('Help')
+
+		# File menu items
+		open_file_action = file_menu.addAction('Open File')
+		close_file_action = file_menu.addAction('Close File')
+		file_menu.addSeparator()
+		exit_app_action = file_menu.addAction('Exit')
+
+		# Help menu items
+		about_app_action = help_menu.addAction('About')
+		other_product_action = help_menu.addAction('Other Products')
+		check_update_action = help_menu.addAction('Check for Update')
+
+		# Menu item actions
+		open_file_action.triggered.connect(self.open_file)
+		close_file_action.triggered.connect(self.close_file)
+		exit_app_action.triggered.connect(self.exit_app)
+		about_app_action.triggered.connect(self.about_app)
+		other_product_action.triggered.connect(self.other_product)
+		check_update_action.triggered.connect(self.check_update)
+
+		# Status bar
+		self.status_bar = self.statusBar()
+		self.status_bar.showMessage('Ready')
+
+		# Tool bar
+		tool_bar = self.addToolBar('File')
+		tool_bar.addAction(open_file_action)
+		tool_bar.addAction(close_file_action)
+		tool_bar.addAction(exit_app_action)
+
+		tool_bar = self.addToolBar('Help')
+		tool_bar.addAction(about_app_action)
+		tool_bar.addAction(other_product_action)
+		tool_bar.addAction(check_update_action)
+
+		# Shortcuts
+		open_file_action.setShortcut('Ctrl+O')
+		close_file_action.setShortcut('Ctrl+W')
+		exit_app_action.setShortcut('Ctrl+Q')
+		about_app_action.setShortcut('Ctrl+I')
+		other_product_action.setShortcut('Ctrl+P')
+		check_update_action.setShortcut('Ctrl+U')
+
+		# Status tip
+		open_file_action.setStatusTip('Open .md file')
+		close_file_action.setStatusTip('Close .md file')
+		exit_app_action.setStatusTip('Exit application')
+		about_app_action.setStatusTip('About application')
+		other_product_action.setStatusTip('Other products')
+
+		# Tool tip
+		open_file_action.setToolTip('Open .md file')
+		close_file_action.setToolTip('Close .md file')
+		exit_app_action.setToolTip('Exit application')
+		about_app_action.setToolTip('About application')
+		other_product_action.setToolTip('Other products')
+
+		# Icon
+		# open_file_action.setIcon(self.style().standardIcon(QStyle.StandardPixmap.Open))
+		# close_file_action.setIcon(self.style().standardIcon(QStyle.StandardPixmap.Close))
+		# exit_app_action.setIcon(self.style().standardIcon(QStyle.StandardPixmap.Exit))
+		# about_app_action.setIcon(self.style().standardIcon(QStyle.StandardPixmap.About))
+		# other_product_action.setIcon(self.style().standardIcon(QStyle.StandardPixmap.Grid))
+		# check_update_action.setIcon(self.style().standardIcon(QStyle.StandardPixmap.Refresh))
+
+		# Window icon
+		# self.setWindowIcon(self.style().standardIcon(QStyle.StandardPixmap.Windows))
+
+	def open_file(self):
+		file_name, _ = QFileDialog.getOpenFileName(self, 'Open .md File', '', 'Markdown Files (*.md)')
 
 		if file_name:
 			with open(file_name, "r", encoding="utf-8") as f:
@@ -61,14 +134,30 @@ class HTMLViewer(QMainWindow):
 
 	def display_html(self, file_name, markdown):
 		html_content = convert_to_html(markdown)
+		html_content = html_content.replace('<img src="', '<img width="100%" src="')
+		html = layout.layout(file_name, html_content)
 
-		html = '<h1>' + file_name + '</h1><div><textarea>' + html_content + '</textarea></div>'
+		self.web_view.setHtml(html)
 
-		self.web_view.setHtml(html_content + html)
+	def close_file(self):
+		self.setWindowTitle('View MD - Markdown Viewer')
+		self.web_view.setHtml('')
+
+	def exit_app(self):
+		self.close()
+
+	def check_update(self):
+		self.status_bar.showMessage('Check for update')
+
+	def other_product(self):
+		self.status_bar.showMessage('Other products')
+
+	def about_app(self):
+		self.status_bar.showMessage('About app')
 
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
-	viewer = HTMLViewer()
+	viewer = ViewMD()
 	viewer.show()
 	sys.exit(app.exec())
